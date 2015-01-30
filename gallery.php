fb6dfaf8c6973db934c739b1e21aad2f53e5f298
Dmitry Demidovsky <d.demidovsky@gmail.com>
2015-01-31 01:48:25 +0300

    Фикс по галерее

--- gallery.php
+++ gallery.php
@@ -28,30 +28,44 @@
 
 
 
-	<div class="b-gallery">
+	<div class="b-gallery-page _mb-l">
 		
 		<div class="row">
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
-			<div class="col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/2.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/3.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/4.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/5.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/6.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/7.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/8.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/9.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/10.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/11.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/12.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/13.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/14.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/15.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
+			<div data-equalheight class="b-gallery-page__item col-xs-4 col-lg-3 _mb-l"><a class="_underline" href="img/content/gallery/large.jpg" data-lightbox="gallery"><img class="_mb-xs" src="img/content/gallery/1.jpg" alt=""><span>Форум «Clouds-NN 2013»</span></a></div>
 		</div>
 
 
 	</div>
 
+
+
+
+		<div class="b-pager">
+			<ul>
+				<li class="_prev"><a href="#" title="Пред. страница"></a></li>
+				<li><a href="#">1</a></li>
+				<li class="_current"><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">5</a></li>
+				<li class="_next"><a href="#" title="След. страница"></a></li>
+			</ul>
+		</div>
 
 
 
