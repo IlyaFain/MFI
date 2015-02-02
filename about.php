d2e25063a3318bbebbdeed2b4411e4db7e6d0460
Dmitry Demidovsky <d.demidovsky@gmail.com>
2015-02-02 14:45:42 +0300

    О компании

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <?
-	define('PAGE_TITLE', 'Пустая страница');
+	define('PAGE_TITLE', 'О компании');
 	define('IS_INNER_PAGE', true);
 ?>
 
@@ -21,9 +21,318 @@
 		<div class="b-intro _inner">
 			<img class="b-intro__logo" src="img/logo/mfi.png" alt="МФИ-Софт">
 			<div class="b-intro__text">
-				<h1>Заголовок</h1>
+				<h1>О компании</h1>
 			</div>
 		</div>
+
+
+		<div class="row b-descr-app _mb-xs">
+		<div class="col-xs-6">
+			<div class="b-descr-app__text _mb-l">
+				<p>Российская инновационная компания, разработчик систем информационной безопасности, систем фильтрации интернет-трафика и систем легального контроля (СОРМ), антифрод-систем.</p>
+				<p>Решения компании предназначены для снижения финансовых и репутационных рисков, связанных с внутренними и внешними угрозами информационной безопасности, а также для расширения коммерческих возможностей операторов связи. В настоящее время различные продукты МФИ Софт используют крупные предприятия, федеральные государственные учреждения и ведомственные структуры, а также операторы связи в России и СНГ.</p>
+				<p>Разработки МФИ Софт в области информационной безопасности и СОРМ применяются с 2003 года. За время работы реализовано более 1500 внедрений решений во всех федеральных округах России</p>
+			</div>
+
+		</div>
+		<div class="col-xs-6">
+			<div class="b-apply b-about-advantages">
+				<h2>Преимущества решений<br>МФИ Софт</h2>
+				<ul>
+					<li>Многопрофильная компания</li>
+					<li>Качество и надежность</li>
+					<li>Масштабируемость и гибкость</li>
+					<li>Экономичность и простота развертывания</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+
+
+
+		<div class="row b-replies _mb-l">
+			<div class="col-xs-12">
+				<h2>Отзывы</h2>
+			</div>
+
+			<div class="col-xs-6">
+				<div class="b-replies__square _red">О продукте «Гарда БД»</div>
+				<h4 class="b-replies__author"><img src="img/logo/cps.png" alt="«CPS»"> Компания «CPS»</h4>
+				<div class="b-replies__text" data-equalheight-any>Существует несколько веских причин для использования «Периметра» в нашем дата-центре. Во-первых, мы считаем, что защита российских ресурсов должна производиться на базе отечественных разработок, соответствующих требованиям местных регуляторов. Во-вторых, для обеспечения клиентов сервисом наивысшего качества. Исходя из этих требований, мы провели отбор претендентов на основе как независимой экспертизы, так и собственного тестирования. Вывод – на данный момент «МФИ Софт» предоставляет лучшее решение на рынке.</div>
+			</div>
+
+			<div class="col-xs-6">
+				<div class="b-replies__square _yellow">О продукте «Гарда БД»</div>
+				<h4 class="b-replies__author"><img src="img/logo/kraftway.png" alt="Kraftway"> Kraftway</h4>
+				<div class="b-replies__text" data-equalheight-any>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed placeat necessitatibus, est. Dolores id reiciendis aut ratione, aliquam doloribus perspiciatis consequatur corporis, sed, quisquam quam sequi minus sint, non. Amet.</div>
+			</div>
+		</div>
+
+
+
+		<div class="b-graph _mb-l">
+			
+			<h2>История</h2>
+
+			<img class="img-responsive" src="img/content/graph.png" alt="">
+
+		</div>
+
+
+
+
+		<div class="b-car__hr _mb-xl">
+
+			<h2 class="_mb-m">Руководство</h2>
+
+			<div class="row">
+
+				<div class="col-xs-6 col-lg-8 _mb-s">
+					<p>Успех работы МФИ Софт обусловлен высоким профессионализмом и уникальной компетенцией руководства компании.</p>
+					<p>Компанию возглавляют эксперты с глубоким знанием потребностей и тенденций развития российского операторского рынка, профессионалы в области разработки и продвижения решений для IP-телефонии, на счету которых сотни успешно реализованных проектов. </p>
+				</div>
+
+
+				<div class="col-xs-6 col-lg-4 _mb-s">
+					<div class="b-car__hr-person">
+						<div class="_clearfix">
+							<div class="b-car__hr-person-photo"><img src="img/content/dir1.jpg" alt=""></div>
+							<div>
+								<br>
+								<h3 class="_mb-xxs">
+									Александр Анатольевич Иванов 
+								</h3>
+								<p class="b-car__hr-person-job">
+									Президент компании
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-6 col-lg-4 _mb-s">
+					<div class="b-car__hr-person">
+						<div class="_clearfix">
+							<div class="b-car__hr-person-photo"><img src="img/content/dir2.jpg" alt=""></div>
+							<div>
+								<br>
+								<h3 class="_mb-xxs">
+									Константин Юрьевич Никашов 
+								</h3>
+								<p class="b-car__hr-person-job">
+									Вице-президент
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-6 col-lg-4 _mb-s">
+					<div class="b-car__hr-person">
+						<div class="_clearfix">
+							<div class="b-car__hr-person-photo"><img src="img/content/dir3.jpg" alt=""></div>
+							<div>
+								<br>
+								<h3 class="_mb-xxs">
+									Олег Иосифович Фаерберг 
+								</h3>
+								<p class="b-car__hr-person-job">
+									Вице-президент
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-6 col-lg-4 _mb-s">
+					<div class="b-car__hr-person">
+						<div class="_clearfix">
+							<div class="b-car__hr-person-photo"><img src="img/content/dir4.jpg" alt=""></div>
+							<div>
+								<br>
+								<h3 class="_mb-xxs">
+									Иван Геннадьевич Смирнов
+								</h3>
+								<p class="b-car__hr-person-job">
+									Генеральный директор, Нижний Новгород
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-6 col-lg-4 _mb-s">
+					<div class="b-car__hr-person">
+						<div class="_clearfix">
+							<div class="b-car__hr-person-photo"><img src="img/content/dir5.jpg" alt=""></div>
+							<div>
+								<br>
+								<h3 class="_mb-xxs">
+									Сергей Владимирович Дрожилкин 
+								</h3>
+								<p class="b-car__hr-person-job">
+									Член Совета директоров
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-6 col-lg-4 _mb-s">
+					<div class="b-car__hr-person">
+						<div class="_clearfix">
+							<div class="b-car__hr-person-photo"><img src="img/content/dir6.jpg" alt=""></div>
+							<div>
+								<br>
+								<h3 class="_mb-xxs">
+									Эдуард Георгиевич Островский
+								</h3>
+								<p class="b-car__hr-person-job">
+									Вице-президент
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-xs-6 col-lg-4 _mb-s">
+					<div class="b-car__hr-person">
+						<div class="_clearfix">
+							<div class="b-car__hr-person-photo"><img src="img/content/dir7.jpg" alt=""></div>
+							<div>
+								<br>
+								<h3 class="_mb-xxs">
+									Александр Александрович Беляков 
+								</h3>
+								<p class="b-car__hr-person-job">
+									Генеральный директор, Москва
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+
+		</div>
+
+
+
+
+
+<div class="b-pressbook _mb-l">
+	<h2>Пресс-бук</h2>
+
+
+	<ul class="row">
+		<li class="col-xs-6 col-lg-4 _mb-m" data-equalheight>
+			<a class="b-pressbook__file-link" href="#"><i class="b-pressbook__file _ai"></i></a>
+			<a class="b-pressbook__file-link" href="#"><i class="b-pressbook__file _jpg"></i></a>
+			<div class="b-pressbook__text">
+				<div>
+					<a class="b-pressbook__title" href="#">Логотип МФИ Софт</a>
+					<div class="b-pressbook__descr">Векторный и растровый форматы логотипа компании для использования в прессе и интернете</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-xs-6 col-lg-4 _mb-m" data-equalheight>
+			<a class="b-pressbook__file-link" href="#"><i class="b-pressbook__file _doc"></i></a>
+			<div class="b-pressbook__text">
+				<div>
+					<a class="b-pressbook__title" href="#">Информация о компании</a>
+					<div class="b-pressbook__descr">Текстовое описание с историей компании, видами деятельности и основными данными</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-xs-6 col-lg-4 _mb-m" data-equalheight>
+			<a class="b-pressbook__file-link" href="#"><i class="b-pressbook__file _zip"></i></a>
+			<div class="b-pressbook__text">
+				<div>
+					<a class="b-pressbook__title" href="#"> Буклет «Продукты МФИ-Софт» </a>
+				</div>
+			</div>
+		</li>
+		<li class="col-xs-6 col-lg-4 _mb-m" data-equalheight>
+			<a class="b-pressbook__file-link" href="#"><i class="b-pressbook__file _zip"></i></a>
+			<div class="b-pressbook__text">
+				<div>
+					<a class="b-pressbook__title" href="#">Буклет «Информационная безопасность»</a>
+				</div>
+			</div>
+		</li>
+	</ul>
+
+</div>
+
+
+
+
+
+
+
+
+<div class="row b-clients _mb-l">
+	<div class="col-xs-12">
+		<h2>Клиенты</h2>
+	</div>
+
+	<div class="col-xs-12 tinyscrollbar tinyscrollbar-horizontal">
+		<div class="row">
+			<div class="col-xs-1">
+				<img src="img/logo/beeline.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/mts.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/rostelekom.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/luk.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/zebra.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/mts.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/rostelekom.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/luk.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/zebra.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/mts.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/rostelekom.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/luk.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/rostelekom.png" alt="">
+			</div>
+			<div class="col-xs-1">
+				<img src="img/logo/luk.png" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+
+
 
 
 
