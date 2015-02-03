db2887c934038fabf96e347f2decc0f027077459
Dmitry Demidovsky <d.demidovsky@gmail.com>
2015-02-03 17:21:08 +0300

    Исправлены подгруппы СОРМ

--- templates/direction_sorm_intro.php
+++ templates/direction_sorm_intro.php
@@ -5,38 +5,44 @@
 					<img class="b-product-intro__logo" src="img/logo/products/sorm.png" alt="СОРМ">
 					<h3><a class="b-product-intro__link" href="#">СОРМ</a></h3>
 					<p>Фильтрация трафика, предупреждение,обнаружение и подавление DDoS-атак </p>
-					<ul class="b-products__sub">
-						<li class="">
-							<div class="b-product-intro__text">
-								<h3><a class="b-product-intro__link" href="#">СОРМович-VoIP</a></h3>
-								<p>Пассивный съем для сетей IP-телефонии</p>
-							</div>
-						</li>
-						<li class="">
-							<div class="b-product-intro__text">
-								<h3><a class="b-product-intro__link" href="#">СОРМович-E1T</a></h3>
-								<p>Осуществляет пассивный съем с Е1-каналов</p>
-							</div>
-						</li>
-						<li class="">
-							<div class="b-product-intro__text">
-								<h3><a class="b-product-intro__link" href="#">ТРОПА</a></h3>
-								<p>Передача данных между несколькими АТС и ПУ</p>
-							</div>
-						</li>
-						<li class="">
-							<div class="b-product-intro__text">
-								<h3><a class="b-product-intro__link" href="#">ТРОПА 3G</a></h3>
-								<p>Обеспечивает работу услуги «Видеовызов»</p>
-							</div>
-						</li>
-						<li class="">
-							<div class="b-product-intro__text">
-								<h3><a class="b-product-intro__link" href="#">Конвертеры LI-СОРМ</a></h3>
-								<p>Для основных видов телефонных&nbsp;коммутаторов</p>
-							</div>
-						</li>
-					</ul>
+					<ul class="b-products__sub _six">
+              <li>
+                <div class="b-product-intro__text">
+                  <h3><a class="b-product-intro__link" href="/direction/sorm/sorm1/konvertery-li-sorm-copy/">Конвертеры LI-СОРМ</a></h3>
+                  <p>Для основных видов телефонных коммутаторов</p>
+                </div>
+              </li>
+              <li>
+                <div class="b-product-intro__text">
+                  <h3><a class="b-product-intro__link" href="/direction/sorm/sorm1/sormovich-e1t-1/">СОРМович-E1T</a></h3>
+                  <p>Пассивный съем с каналов E1</p>
+                </div>
+              </li>
+              <li>
+                <div class="b-product-intro__text">
+                  <h3><a class="b-product-intro__link" href="/direction/sorm/sorm1/sormovich-voip/">СОРМович-VoIP</a></h3>
+                  <p>Пассивный съем для сетей IP-телефонии</p>
+                </div>
+              </li>
+              <li>
+                <div class="b-product-intro__text">
+                  <h3><a class="b-product-intro__link" href="/direction/sorm/sorm1/tropa/">Тропа</a></h3>
+                  <p>Для передачи данных между несколькими АТС и ПУ</p>
+                </div>
+              </li>
+              <li>
+                <div class="b-product-intro__text">
+                  <h3><a class="b-product-intro__link" href="/direction/sorm/sorm1/tropa-3g/">Тропа-3G</a></h3>
+                  <p>Для услуги "Видеовызов" в GSM-сетях</p>
+                </div>
+              </li>
+              <li>
+                <div class="b-product-intro__text">
+                  <h3><a class="b-product-intro__link" href="/direction/sorm/sorm1/konvertor-70268/">Конвертор 70/268</a></h3>
+                  <p>для адаптации коммутаторов к стандартам ПУ ОРМ</p>
+                </div>
+              </li>
+            </ul>
 				</div>
 			</div>
 		</div>
