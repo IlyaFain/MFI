0193b648b678b84864a9c36155cbe5ec776353dc
Dmitry Demidovsky <d.demidovsky@gmail.com>
2015-02-13 16:31:00 +0300

    Доп. класс для "двойной" картики продукта

--- templates/direction_garda_intro.php
+++ templates/direction_garda_intro.php
@@ -1,17 +1,17 @@
 	<div class="row">
 		<div class="col-xs-6">
-			<div class="b-product-intro">
+			<div class="b-product-intro _wide-pic">
 				<div class="b-product-intro__text _border-right">
-					<img class="b-product-intro__logo" src="img/logo/products/garda-bd.png" alt="ГАРДА БД">
-					<h3><a class="b-product-intro__link" href="#">ГАРДА БД</a></h3>
+					<img class="b-product-intro__logo" src="img/logo/products/GardaBD_par_110.jpg" alt="ГАРДА БД">
+					<h3><a class="b-product-intro__link" href="#">Защита от утечек информации</a></h3>
 					<p>Российская инновационная компания, разработчик систем информационной безопасности (системы DLP, защиты от&nbsp;DDoS), систем фильтрации интернет-трафика и&nbsp;систем легального контроля (СОРМ), антифрод-систем.</p>
 				</div>
 			</div>
 		</div>
 		<div class="col-xs-6">
-			<div class="b-product-intro">
+			<div class="b-product-intro _wide-pic">
 				<div class="b-product-intro__text">
-					<img class="b-product-intro__logo" src="img/logo/products/garda.png" alt="ГАРДА ПРЕДПРИЯТИЕ">
+					<img class="b-product-intro__logo" src="img/logo/products/GardaEnt_110.jpg" alt="ГАРДА ПРЕДПРИЯТИЕ">
 					<h3><a class="b-product-intro__link" href="#">ГАРДА ПРЕДПРИЯТИЕ</a></h3>
 					<p>Российская инновационная компания, разработчик систем информационной безопасности (системы DLP, защиты от&nbsp;DDoS), систем фильтрации интернет-трафика и&nbsp;систем легального контроля (СОРМ), антифрод-систем.</p>
 				</div>
