f927324aea7ab660c7086ba5ad5d5c8bd9bce29e
Dmitry Demidovsky <d.demidovsky@gmail.com>
2015-02-24 18:05:19 +0300

    Превьюшки новостей

--- articles_list.php
+++ articles_list.php
@@ -21,7 +21,7 @@
 		<div class="b-articles-list _mb-l">
 			<h2 class="_mb-m">Статьи</h2>
 
-			<ul class="b-news__list _articles _big-text">
+			<ul class="b-news__list _preview-always _articles _big-text">
 				<li>
 					<img alt="" src="img/content/articles/1.jpg" class="b-news__img">
 					<div class="b-news__pad">
